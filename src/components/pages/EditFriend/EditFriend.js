b7d10577363149d82bb8d49028083131d87a1fc6
Jasmine Walters <jsmnwltrs@gmail.com>
2019-01-09 21:00:43 -0600

    setup routing for all components

---
+++ src/components/pages/EditFriend/EditFriend.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './EditFriend.scss';
+
+class EditFriend extends React.Component {
+  render() {
+    return (
+      <div className="EditFriend">
+       <h2>Edit Friend</h2>
+      </div>
+    );
+  }
+}
+
+export default EditFriend;
---
+++ src/components/pages/HolidayDetail/HolidayDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HolidayDetail.scss';
+
+class HolidayDetail extends React.Component {
+  render() {
+    return (
+      <div className="HolidayDetail">
+       <h2>Holiday Details</h2>
+      </div>
+    );
+  }
+}
+
+export default HolidayDetail;
