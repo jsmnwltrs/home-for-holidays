d9a31084fde7659510415739b76e7ded84d9d678
Jasmine Walters <jsmnwltrs@gmail.com>
2019-01-09 19:32:40 -0600

    setup auth and simple routing

---
+++ src/components/MyNavbar/MyNavbar.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './MyNavbar.scss';
+import PropTypes from 'prop-types';
+import { NavLink as RRNavLink } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+class MyNavbar extends React.Component {
+  static propTypes = {
+    isAuthed: PropTypes.bool,
+    logoutClickEvent: PropTypes.func,
+  }
+
+  state = {
+    isOpen: false,
+  };
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  render() {
+    const { isAuthed, logoutClickEvent } = this.props;
+    const buildNavbar = () => {
+      if (isAuthed) {
+        return (
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink tag={RRNavLink} to='/home'>Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink onClick={logoutClickEvent}>Logout</NavLink>
+          </NavItem>
+        </Nav>
+        );
+      }
+      return <Nav className="ml-auto" navbar />;
+    };
+
+    return (
+      <div className="my-navbar sticky-top">
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/">Home for the Holidays</NavbarBrand>
+          <NavbarToggler onClick={e => this.toggle(e)} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            {buildNavbar()}
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default MyNavbar;
